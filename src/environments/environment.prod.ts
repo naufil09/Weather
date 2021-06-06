833bd33fea73c9c7bb1843da47b0be77fdf9b99c
naufil09 <snaufil09@gmail.com>
2021-06-06 10:35:33 +0530

    Ignore environment.

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: "",
-  ANGULAR_API_KEY: "",
-  ANGULAR_API_HOST: "",
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiURL: "",
-  ANGULAR_API_KEY: "",
-  ANGULAR_API_HOST: "",
-};
